f83123c05607bc32a0eb2d7d0fa275efe32dd97b
Dmitry Ovchinnikov <dimatt2013@gmail.com>
2020-06-13 13:04:11 +0500

    completed project

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,107 @@
+const project_folder = 'dist';
+const source_folder = 'src';
+const path = {
+	build: {
+		html: './' + project_folder + '/',
+		css: project_folder + '/css/',
+		img: project_folder + '/img/',
+		inject: './dist/css/*.min.css',
+	},
+	src: {
+		html: './' + source_folder + '/index.html',
+		css: source_folder + '/sass/*.sass',
+		img: source_folder + '/img/**/*.{jpg,png,svg,gif,ico,webp}',
+	},
+	watch: {
+		html: source_folder + '/**/*.html',
+		css: source_folder + '/sass/**/*.sass',
+		img: source_folder + '/img/**/*.{jpg,png,svg,gif,ico,webp}',
+	},
+	clean: './' + project_folder + '/',
+};
+
+const { src, dest } = require('gulp');
 const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
+const browsersync = require('browser-sync').create();
+const del = require('del');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const groupmedia = require('gulp-group-css-media-queries');
+const cleancss = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const imagemin = require('gulp-imagemin');
+const inject = require('gulp-inject');
 
-// Compile sass into CSS, auto-inject into browsers, auto-prefixes + concatination sass(css)'s files
-function style(done) {
-	gulp
-		.src(['src/sass/*.sass', '!src/sass/_*.sass'])
+function browserSync(params) {
+	browsersync.init({
+		server: {
+			baseDir: './' + project_folder + '/',
+		},
+		port: 3000,
+		notify: false,
+	});
+}
+
+function html() {
+	return src(path.src.html)
+		.pipe(dest(path.build.html))
+		.pipe(inject(src(path.build.inject, { read: false }), { relative: true }))
+		.pipe(dest(path.build.html))
+		.pipe(browsersync.stream());
+}
+
+function images() {
+	return src(path.src.img)
 		.pipe(
-			sass({
-				errLogToConsole: true,
+			imagemin({
+				progressive: true,
+				svgoPlugins: [{ removeViewBox: false }],
+				interlaced: true,
+				optimizationLevel: 3,
 			})
 		)
-		.on('error', console.error.bind(console))
-		.pipe(autoprefixer({ cascade: false }))
-		.pipe(gulp.dest('src/css'))
-		.pipe(browserSync.stream());
-	done();
+		.pipe(dest(path.build.img))
+		.pipe(browsersync.stream());
 }
 
-// Static Server + watching scss/html files
-function watch(done) {
-	browserSync.init({
-		server: 'src/',
-	});
-	gulp.watch('src/sass/*.sass', style);
-	gulp.watch('src/*.html').on('change', () => {
-		browserSync.reload();
-		done();
-	});
-	gulp.watch('src/js/*.js').on('change', () => {
-		browserSync.reload();
-		done();
-	});
-	done();
+function css() {
+	return src(path.src.css)
+		.pipe(
+			sass({
+				outputStyle: 'expanded',
+			}).on('error', sass.logError)
+		)
+		.pipe(groupmedia())
+		.pipe(
+			autoprefixer({
+				overrideBrowserslist: ['last 5 versions'],
+				cascade: true,
+			})
+		)
+		.pipe(dest(path.build.css))
+		.pipe(cleancss())
+		.pipe(rename({ extname: '.min.css' }))
+		.pipe(dest(path.build.css))
+		.pipe(browsersync.stream());
 }
 
-exports.default = gulp.series(watch, style);
+function injectFiles() {
+	return src(path.src.html).pipe(dest(path.build.html));
+}
+
+function watchFiles(params) {
+	gulp.watch([path.watch.html], html);
+	gulp.watch([path.watch.css], css);
+	gulp.watch([path.watch.img], images);
+}
+
+function clean(params) {
+	return del(path.clean);
+}
+
+const build = gulp.series(clean, gulp.parallel(css, images), html);
+const watch = gulp.parallel(build, watchFiles, browserSync);
+
+exports.watch = watch;
+exports.build = build;
+exports.default = watch;
